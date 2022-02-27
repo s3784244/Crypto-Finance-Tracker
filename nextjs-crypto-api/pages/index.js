955124b98b3c951079e85976be779c338b71b059
Kajal Soni <s3784244@student.rmit.edu.au>
2022-02-27 19:40:04 +1100

    Added CoinList API

---
+++ nextjs-crypto-api/pages/index.js
@@ -0,0 +1,30 @@
+import Head from 'next/head'
+import SearchBar from '../components/SearchBar'
+import CoinList from '../components/CoinList'
+
+export default function Home({ filteredCoins }) {
+  return (
+    <div>
+      <Head>
+        <title>Title</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <SearchBar type="text" placeholder='Search'/>
+      <CoinList filteredCoins={filteredCoins} />
+
+    </div>
+  );
+}
+
+export const getServerSideProps = async () => {
+  const res = await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=aud&order=market_cap_desc&per_page=10&page=1&sparkline=false')
+
+  const filteredCoins = await res.json()
+
+  return {
+    props: {
+      filteredCoins
+    }
+  }
+}
